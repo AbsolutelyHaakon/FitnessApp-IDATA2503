27f37857eb0977e64eed21f5c3635389f0037947
HSK <haakonsk@live.no>
2024-09-27 09:46:37 +0200

    Small changes to structure

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,2 +1,25 @@
 import 'package:flutter/material.dart';
 
+class Home extends StatefulWidget{
+  const Home({super.key});
+
+  @override
+  State<Home> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Home page!'),
+      ),
+    );
+  }
+}
+
