71a8bb2d7c420e2d745eb37dee2dac8ba5b93180
HSK <haakonsk@live.no>
2024-09-27 09:50:26 +0200

    Fixed small launch issue

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:fitnessapp_idata2503/pages/home.dart';
 
 void main() {
-  runApp(const Home());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Home(),
+    );
+  }
 }
