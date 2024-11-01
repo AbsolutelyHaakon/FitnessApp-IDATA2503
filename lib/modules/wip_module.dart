18f7134ab65654102d7821e87641c7aaaa9e6f83
HSK <haakonsk@live.no>
2024-11-01 18:05:43 +0100

    fixed wip module

--- lib/modules/wip_module.dart
+++ lib/modules/wip_module.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitnessapp_idata2503/pages/create_workout_page.dart';
 import 'package:fitnessapp_idata2503/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fitnessapp_idata2503/pages/account_setup.dart';
 
+import '../pages/exercise_selector.dart';
 import '../styles.dart';
 
 // Module for displaying a WIP widget
@@ -13,7 +15,14 @@ import '../styles.dart';
 // Last edited: 31/10/2024
 // Last edited by: Håkon Svensen Karlsen
 
-class WipModule extends StatelessWidget {
+class WipModule extends StatefulWidget {
+  final User? user;
+  const WipModule({super.key, this.user});
+  @override
+  State<WipModule> createState() => _WipModuleState();
+}
+
+class _WipModuleState extends State<WipModule> {
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -24,7 +33,7 @@ class WipModule extends StatelessWidget {
             context,
             PageRouteBuilder(
               pageBuilder: (context, animation, secondaryAnimation) =>
-                  const CreateWorkoutPage(),
+                   ExerciseSelectorPage(user: widget.user),
               transitionsBuilder:
                   (context, animation, secondaryAnimation, child) {
                 const begin = Offset(1.0, 0.0);
