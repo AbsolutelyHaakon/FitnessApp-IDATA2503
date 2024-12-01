7c46630cee1b56352994fab5d08c297f75dd4fec
HSK <haakonsk@live.no>
2024-12-01 11:39:21 +0100

    Removed bg color

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -90,47 +90,41 @@ class _HomeState extends State<Home> {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 5.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color(0xFF262825),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    left: 10.0, right: 10.0, top: 20.0, bottom: 40.0),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10.0),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text('Today\'s workout',
-                            style: Theme.of(context).textTheme.headlineMedium),
-                      ),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 10.0, right: 10.0, top: 20.0, bottom: 40.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text('Today\'s workout',
+                          style: Theme.of(context).textTheme.headlineMedium),
                     ),
-                    const SizedBox(height: 10),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: AppColors.fitnessModuleColor,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Column(
-                        children: [
-                          HomePageCalendar(onRefresh: fetchNextWorkout, workoutsIsEmpty: workouts.isEmpty),
-                          const SizedBox(height: 10),
-                          if (workouts.isNotEmpty)
-                            WorkoutsBox(
-                              workouts: [...workouts],
-                              isHome: true,
-                              isSearch: false,
-                            ),
-                        ],
-                      ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: AppColors.fitnessModuleColor,
+                      borderRadius: BorderRadius.circular(15),
                     ),
-                    const SizedBox(height: 20),
-                    const RingsModule(),
-                  ],
-                ),
+                    child: Column(
+                      children: [
+                        HomePageCalendar(onRefresh: fetchNextWorkout, workoutsIsEmpty: workouts.isEmpty),
+                        const SizedBox(height: 10),
+                        if (workouts.isNotEmpty)
+                          WorkoutsBox(
+                            workouts: [...workouts],
+                            isHome: true,
+                            isSearch: false,
+                          ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const RingsModule(),
+                ],
               ),
             ),
           ),
